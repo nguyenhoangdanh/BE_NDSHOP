b8867ca02f605bf85ca13875afc4b0e651ad9b0a
= <=>
2023-10-07 12:44:38 +0700

    update dotenv

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ process.on("uncaughtException", (err) => {
 // config
 if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({
-      path: "backend/config/.env",
+      path: "config/.env",
     });
   }
 
